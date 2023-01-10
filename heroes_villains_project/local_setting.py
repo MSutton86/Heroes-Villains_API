d27d52561d639c92fbe852f43f5dd5baec9a92ba
Mark S <stcoronawraps@gmail.com>
2023-01-10 12:59:44 -0700

    Started project. Added Packages, mysql, django, + pipenv. Created local_settings

---
+++ heroes_villains_project/local_setting.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'heroes_villains_project',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Codepass1!',
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-@f&f4e19c)$pryq+q%=g5)(v+8=)8pv1%krm5q(sx$zwwdjrtq'
